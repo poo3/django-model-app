193a29f24bd01bae60c0c906e5abaa1fae913f97
poo3fuma <coolishpoo3@gmail.com>
2022-02-26 13:49:12 +0900

    Change SECRET_KEY

--- ModelProject/settings.py
+++ ModelProject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@cwg@#y3s+tx#(%nn1!67prs!fco)!4rd%%&2^5-4w91rp$e#u'
+from .local_settings import SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
