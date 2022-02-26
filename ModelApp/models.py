fc03386fea46606c13d9d612b06be569535d0bed
poo3fuma <coolishpoo3@gmail.com>
2022-02-26 13:37:28 +0900

    Add person model

--- ModelApp/models.py
+++ ModelApp/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-class Person(models.Model):
+class BaseMeta(models.Model):
+  create_at = models.DateField(default=timezone.datetime.now)
+  update_at = models.DateField(default=timezone.datetime.now)
+  
+  class Meta:
+    abstract = True
+
+class Person(BaseMeta):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   birthday = models.DateField(default='1900-01-01')
@@ -10,4 +17,8 @@ class Person(models.Model):
   saraly = models.FloatField(null=True)
   memo = models.TextField()
   web_site = models.URLField(null=True,blank=True)
-  create_at = models.DateTimeField(default=timezone.datetime.now)
+
+  class Meta:
+    db_table = 'person'
+    index_together = [['first_name','last_name']]
+    ordering = ['saraly']
