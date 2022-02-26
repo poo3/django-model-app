6d9a157f40cffc79fe0d88b8d70324b95ba8313a
poo3fuma <coolishpoo3@gmail.com>
2022-02-26 11:52:54 +0900

    Make migration for adding sales model

--- ModelApp/models.py
+++ ModelApp/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Person(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
+  
+class Sales(models.Model):
+  fee = models.IntegerField()
