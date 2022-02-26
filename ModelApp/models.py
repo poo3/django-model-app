d25c05b65296bb7a7540c6f46e7ccd925162124a
poo3fuma <coolishpoo3@gmail.com>
2022-02-26 13:24:50 +0900

    Add person model

--- ModelApp/models.py
+++ ModelApp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Person(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
-  
-class Sales(models.Model):
-  fee = models.IntegerField()
-  create_at = models.DateTimeField()
-  update_at = models.DateTimeField()
+  birthday = models.DateField(default='1900-01-01')
+  email = models.EmailField(db_index=True)
+  saraly = models.FloatField(null=True)
+  memo = models.TextField()
+  web_site = models.URLField(null=True,blank=True)
+  create_at = models.DateTimeField(default=timezone.datetime.now)
